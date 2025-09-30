eb7a0863ff38f5fc825d2815f8271aaeff1ed711
Sowon Kim <sso626262@naver.com>
2025-09-30 23:58:39 +0900

    feat: react-force-graph 세팅 및 가중치 그래프 뷰 UI 1차 구현 (#4)

--- src/app/components/graph/WeightGraphPanel.tsx
+++ src/app/components/graph/WeightGraphPanel.tsx
@@ -1,11 +1,12 @@
 import { KeywordSearch } from './KeywordSearch'
+import { WeightGraphView } from './WeightGraphView'
 
 export const WeightGraphPanel = () => {
     return (
         <div className="w-full">
             <KeywordSearch />
-            <div className="text-3xl font-bold">WING</div>
-            WeightGraphPanel
+            <div className="text-3xl font-bold">TEST</div>
+            <WeightGraphView />
         </div>
     )
 }
