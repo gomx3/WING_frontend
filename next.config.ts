a1f9613542c5e5f240322976ee272c3aae9884cb
Sowon Kim <sso626262@naver.com>
2025-09-30 23:57:46 +0900

    feat: 아티클 패널 UI 1차 구현 (#1)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+    /* config options here */
+    images: {
+        domains: ['vanilla-crop-0ed.notion.site'], // 노션 페이지 이미지 임시 허용
+    },
+}
 
-export default nextConfig;
+export default nextConfig
--- src/app/components/articles/ArticlePanel.tsx
+++ src/app/components/articles/ArticlePanel.tsx
@@ -1,3 +1,27 @@
+import { ArticleItem } from './ArticleItem'
+
 export const ArticlePanel = () => {
-    return <div className="w-full border-l border-white-600">ArticlePanel</div>
+    return (
+        <div className="w-full max-w-md p-3 space-y-8 border-l border-white-600">
+            <div>
+                <p className="px-2 py-1 rounded-[4px] w-fit text-[0.8rem] tracking-[-0.4px] text-primary-600 border border-white-500">
+                    최근 3개월
+                </p>
+                <h3 className="font-bold text-[1.4rem]">{`노드 A <-> 노드 B 관련 기사`}</h3>
+                <p className="text-[0.9rem] tracking-[-0.45px] text-gray-300">
+                    최근 3개월 동안 35개의 기사에서 함께 언급되었습니다.
+                </p>
+            </div>
+
+            <hr className="border-gray-800" />
+
+            <div className="space-y-6">
+                <ArticleItem />
+                <ArticleItem />
+                <ArticleItem />
+                <ArticleItem />
+                <ArticleItem />
+            </div>
+        </div>
+    )
 }
